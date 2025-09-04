759f7aadac52686ccf89ccd060e10abc0d6221d9
AlexM212001 <amirm.ab20@gmail.com>
2025-09-04 14:20:38 -0400

    Lab 1

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -9,24 +9,50 @@ See documentation here: https://www.raylib.com/, and examples here: https://www.
 #include "raygui.h"
 
 const unsigned int TARGET_FPS = 50;
+float dt = 1.0f / TARGET_FPS;
 float time = 0;
+float x = 500;
+float y = 500;
+float frequency = 1.0f;
+float amplitude = 100.0f;
+
+void Update()
+{
+    dt = 1.0f / TARGET_FPS;
+    time += dt;
+
+	x = x + (-sin(time * frequency)) * frequency * amplitude * dt;
+    y = y + (cos (time * frequency)) * frequency * amplitude * dt;
+
+}
+void Draw()
+{
+    BeginDrawing();
+    ClearBackground(BLACK);
+    DrawText("game 2005 101541841 Amir", 10, float(GetScreenHeight() - 30), 20, LIGHTGRAY);
+
+    GuiSliderBar(Rectangle{ 60, 5, 1000, 10 }, "Time", TextFormat("%.2f", time), &time, 0, 240);
+    DrawText("Time", GetScreenWidth() - 150, 10, 30, LIGHTGRAY);
+
+	DrawCircle(x, y, 70, RED);
+	DrawCircle(500 + cos(time * frequency) * amplitude, 500 + sin(time * frequency) * amplitude, 70, BLUE);
+
+    EndDrawing();
+
+}
+
+
+
+
 int main()
 {
-    InitWindow(1200, 800, "Physics-1");
+    InitWindow(1200, 800, "game 2005 101541841 Amir ");
     SetTargetFPS(TARGET_FPS);
 
     while (!WindowShouldClose())
     {
-        BeginDrawing();
-            ClearBackground(WHITE);
-            DrawText("Hello world!", 10, 10, 20, LIGHTGRAY);
-
-
-            time += 1;
-            GuiSliderBar(Rectangle{ 60, 5, 1000, 10 }, "Time", TextFormat("%.2f", time), &time, 0, 240);
-
-
-        EndDrawing();
+		Update();
+		Draw();   
     }
 
     CloseWindow();
