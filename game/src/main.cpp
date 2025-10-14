ccd16c792a4667119802fce7be490e235dd9cda2
AlexM212001 <amirm.ab20@gmail.com>
2025-10-14 13:54:09 -0400

    Lab 3

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -41,11 +41,45 @@ public:
     Vector2 velocity = { 0,0 };
     float mass = 1;
 
-    float radius = 15; 
+    //float radius = 15;
     std::string name = "object";
     Color color = RED;
+	virtual void draw()
+	{
+		DrawCircle(position.x, position.y, 2, color);
+		DrawText(name.c_str(), position.x, position.y, 12, LIGHTGRAY);
+		
+	}
+  
+};
 
-    void draw()
+class PhysicBox : public PhysicObject
+{
+   // Vector2 position = { 0,0 };
+    //Vector2 velocity = { 0,0 };
+    //float mass = 1;
+
+	//float Hight ;
+	//float height ;
+    //std::string name = "object";
+   
+    //Color color = RED;
+public:
+	Vector2 size ;
+
+	void draw()
+	{
+		DrawRectangleV(position, size, color);
+		DrawText(name.c_str(), position.x, position.y, 12, LIGHTGRAY);
+		DrawLineEx(position, position + velocity, 1, color);
+	}
+};
+class PhysicCircle : public PhysicObject
+{
+public:
+	float radius;
+    Color originalColor = GREEN;
+    void draw() override
     {
         DrawCircle(position.x, position.y, radius, color);
 
@@ -54,17 +88,35 @@ public:
         DrawLineEx(position, position + velocity, 1, color);
     }
 };
+
+bool circleOverlaps(PhysicCircle*circleA, PhysicCircle*circleB)
+{
+	Vector2 displacementFromAToB = circleB->position - circleA->position;
+	float distance = Vector2Length(displacementFromAToB);
+	float sumOfRadii = circleA->radius + circleB->radius;
+	if (sumOfRadii> distance)
+	{
+		return true;
+	}
+    else
+		return false;
+
+   
+}
+
+
 class PhysicWorld
 {
 private:
     unsigned int ObjectCount = 0;
 public:
-    std::vector<PhysicObject> Object; // All objects in physics simulation
+    std::vector<PhysicObject*> Object; // All objects in physics simulation
+    std::vector<PhysicBox*> Boxes;
     Vector2 accelerationGravity = { 0, 9 };
 
-    void add(PhysicObject newObject) // Add to physics simulation
+    void add(PhysicObject* newObject) // Add to physics simulation
     {
-        newObject.name = std::to_string(ObjectCount);
+        newObject->name = std::to_string(ObjectCount);
         Object.push_back(newObject);
         ObjectCount++;
     }
@@ -74,13 +126,46 @@ public:
     {
         for (int i = 0; i < Object.size(); i++)
         {
-            
-            Object[i].position = Object[i].position + Object[i].velocity * dt;
+			PhysicObject* obj = Object[i];
+
+
+            obj->position = obj->position + obj->velocity * dt;
            
 
-            Object[i].velocity = Object[i].velocity + accelerationGravity * dt;
+            obj->velocity = obj->velocity + accelerationGravity * dt;
         }
+		checkCollisions();
+    }
 
+    void checkCollisions()
+    {
+		//Reset all colors
+        for (auto& obj : Object) {
+            PhysicCircle* circle = dynamic_cast<PhysicCircle*>(obj);
+            if (circle) circle->color = circle->originalColor;
+        }
+		//changing color if overlapping
+        for (int i = 0; i < Object.size(); i++)
+        {
+            PhysicCircle* circlePointerA = dynamic_cast<PhysicCircle*>(Object[i]);
+            if (!circlePointerA) continue;
+
+
+            for (int j = i + 1; j < Object.size(); j++)
+            {
+                PhysicCircle* circlePointerB = dynamic_cast<PhysicCircle*>(Object[j]);
+                if (!circlePointerB) continue;
+
+                if (circleOverlaps(circlePointerA, circlePointerB))
+                {
+                    circlePointerA->color = RED;
+                    circlePointerB->color = RED;
+                }
+            }
+
+
+        }
+        
     }
 };
 PhysicWorld world;
@@ -90,15 +175,20 @@ void cleanup()
     // checking if the object is offscreen!
     for (int i = 0; i < world.Object.size(); i++)
     {
+        PhysicObject* obj = world.Object[i];
+
         //Is it offscreen?
-        if (world.Object[i].position.y > GetScreenHeight()
-            || world.Object[i].position.y < 0
-            || world.Object[i].position.x > GetScreenWidth()
-            || world.Object[i].position.x < 0
+        if (  obj->position.y > GetScreenHeight()
+            || obj->position.y < 0
+            || obj->position.x > GetScreenWidth()
+            || obj->position.x < 0
             )
         {
             //Destroy!
-            world.Object.erase(world.Object.begin() + i);
+			auto iterator = world.Object.begin() + i;
+			PhysicObject* pointerToPhysicObject = *iterator;
+			delete pointerToPhysicObject;
+            world.Object.erase(iterator);
             i--;
         }
     }
@@ -114,14 +204,16 @@ void update()
 
 	if (IsKeyPressed(KEY_SPACE))
 	{
-		PhysicObject newBird;
-        newBird.position = launchPosition;
-		newBird.velocity = { launchSpeed * (float)cos(launchAngle * DEG2RAD), -launchSpeed * (float)sin(launchAngle * DEG2RAD) };
+		PhysicCircle* newBird = new PhysicCircle();//new kryword
+        newBird->position = launchPosition;
+		newBird->velocity = { launchSpeed * (float)cos(launchAngle * DEG2RAD), -launchSpeed * (float)sin(launchAngle * DEG2RAD) };
 		
 		
-		newBird.radius = (rand() % 26) + 5; 
-		Color randomColor = {rand() % 256 , rand() % 256, rand() % 256, 255};
-		newBird.color = randomColor;
+		newBird->radius = (rand() % 26) + 5; 
+		//Color randomColor = {rand() % 256 , rand() % 256, rand() % 256, 255};
+		//newBird->color = randomColor;
+        newBird->color = GREEN;
+        newBird->originalColor = GREEN;
 
 		world.add(newBird);
 	}
@@ -151,7 +243,7 @@ void Draw()
     //Draw all physics objects!
     for (int i = 0; i < world.Object.size(); i++)
     {
-        world.Object[i].draw();
+        world.Object[i]->draw();
     }
 
     EndDrawing();
